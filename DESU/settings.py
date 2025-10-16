3eddfe3226b9878dec6cdad08932dbecba2103bd
lucacontalbo <luca.contalbo@gmail.com>
2025-10-16 11:23:38 +0200

    fixed settings

--- DESU/settings.py
+++ DESU/settings.py
@@ -28,12 +28,15 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 STATIC_URL = '/activity/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL_BASE = '/static/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://dbgroup.ing.unimore.it",
+]
 
 # Application definition
 
@@ -83,7 +86,7 @@ WSGI_APPLICATION = 'DESU.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db" / 'db.sqlite3',
     }
 }
 
@@ -122,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
